8a2850471dc3574ca2a8642399af12c754100398
Lengdons <kleinbergsrihards@gmail.com>
2026-01-08 15:00:51 +0200

    JOP1

---
+++ src/test1/Throwaway.java
@@ -0,0 +1,35 @@
+package test1;
+
+import javax.swing.JOptionPane;
+
+public class Throwaway {
+
+		public static void veidot() {
+			StringBuilder build = new StringBuilder();
+			for (int i=0; i<10; i++) {
+				for (int j=0; j<10; j++) {
+					build.append("⬜ \t");
+				}
+				build.append("\n");
+			}
+			JOptionPane.showMessageDialog(null, build.toString());
+		}
+		
+		public static void izveleties() {
+			String a = JOptionPane.showInputDialog(null, "1 - Vieglu 2 - Videju 3 - Grutu");
+			
+		}
+		
+		public static void main(String[] args) {
+			String a = JOptionPane.showInputDialog(null, "1 - Veidot savu crossword \n2 - Izveleties gatavu crossword");
+			
+			switch (a) {
+			case "1": 
+				veidot();
+			break;
+			case "2":
+				izveleties();
+			break;
+		}
+	}
+	}
