593bed5f8fa5837b4dcf2a74dc3420913a214727
Lengdons <kleinbergsrihards@gmail.com>
2026-01-12 16:41:45 +0200

    Logic Engine Collision check Vertical2

--- src/core/LogicEngine.java
+++ src/core/LogicEngine.java
@@ -4,24 +4,42 @@ import java.util.Scanner;
 
 public class LogicEngine {
 	char[][] board = new char[15][15];
-	
-	
-
-public void LogicEngine() {
 	Scanner sc = new Scanner(System.in);
-	System.out.println("vārds");
 	String a = sc.next();
 	char[]letters = a.toCharArray();
 	
+
+public void LogicEngine() {	
 	for(int i=0; i<15; i++) {
 		for(int j=0; j<15; j++) {
 			board[i][j] = '-';
 			}	
 		}
-	board[0][0] = 'J';
+//horizontal loop
+	for(int j=0;j<a.length();j++) {
+		board[0+5][j+2] = letters[j];
 	}
+//vetical loop
+	for(int i=0; i<a.length(); i++) {
+		board[i+5][0+2] = letters[i];
+	}		
+}
+public boolean canPlaceVertical(String word, int startRow, int startCol) {
+	//konktrēta collison loop
 	
+		for(int i=0; i<word.length(); i++) {
+			if(board[startRow+i][startCol] != word.charAt(i) && board[startRow+i][startCol] != '-'){	
+				return false;}
+				}
+		if(startRow+word.length()<15)(board[startRow+word.length()][startCol] != '-') {
+			return false;
+		}else{
+		return true;
+		}
 	}
+
+}
+
 	
 
 
