75006b195a332b885d90234b1bc525368fb48714
Lengdons <kleinbergsrihards@gmail.com>
2026-01-12 16:57:43 +0200

    Logic Engine Collision check Horizontal

--- src/core/LogicEngine.java
+++ src/core/LogicEngine.java
@@ -3,15 +3,15 @@ package core;
 import java.util.Scanner;
 
 public class LogicEngine {
-	char[][] board = new char[15][15];
+	char[][] board = new char[20][20];
 	Scanner sc = new Scanner(System.in);
 	String a = sc.next();
 	char[]letters = a.toCharArray();
 	
-
+// Grid/board 20x20
 public void LogicEngine() {	
-	for(int i=0; i<15; i++) {
-		for(int j=0; j<15; j++) {
+	for(int i=0; i<20; i++) {
+		for(int j=0; j<20; j++) {
 			board[i][j] = '-';
 			}	
 		}
@@ -25,18 +25,40 @@ public void LogicEngine() {
 	}		
 }
 public boolean canPlaceVertical(String word, int startRow, int startCol) {
-	//konktrēta collison loop
-	
+	//Vertikālais collision ckeck loop
+	if(word.length()+startRow<board.length){
 		for(int i=0; i<word.length(); i++) {
 			if(board[startRow+i][startCol] != word.charAt(i) && board[startRow+i][startCol] != '-'){	
 				return false;}
 				}
-		if(startRow+word.length()<15)(board[startRow+word.length()][startCol] != '-') {
+		if(startRow+word.length()<board.length && board[startRow+word.length()][startCol] != '-') {
 			return false;
 		}else{
-		return true;
+			return true;
+			}
+		}else{
+			return false;
 		}
 	}
+
+public boolean canPlaceHorizontal(String word, int startRow, int startCol) {
+	//Horizontāli collision ckeck loop
+	if(word.length()+startCol<board.length){
+		for(int i=0; i<word.length(); i++){
+			if(board[startRow][startCol+i] != word.charAt(i) && board[startRow][startCol+i] != '-'){	
+				return false;}
+				}
+		if(startCol+word.length()<board.length && board[startRow][startCol+word.length()] != '-') {
+			return false;
+		}else{
+			return true;
+			}
+		}else{
+			return false;
+		}
+	}
+
+
 
 }
 
