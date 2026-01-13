9e0a76e237ee9f7506632935ac6b9f82dfdb8d76
Lengdons <kleinbergsrihards@gmail.com>
2026-01-13 23:04:04 +0200

    placedWord - Open

--- src/core/placedWord.java
+++ src/core/placedWord.java
@@ -1,5 +1,16 @@
 package core;
 
+// konteineris vārdam
 public class placedWord {
-
+	public String word;
+    public int row;
+    public int col;
+    public boolean isVertical;
+    
+    public placedWord(String word, int row, int col, boolean isVertical) {
+        this.word = word;
+        this.row = row;
+        this.col = col;
+        this.isVertical = isVertical;
+    }
 }
