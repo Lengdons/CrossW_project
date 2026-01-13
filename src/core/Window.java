dc2193904551d52d35bc761ccf2a5a355463eb2d
Lengdons <kleinbergsrihards@gmail.com>
2026-01-13 18:41:13 +0200

    Window - Lauciņu izveide

--- src/core/Window.java
+++ src/core/Window.java
@@ -1,6 +1,9 @@
 package core;
 
+import java.awt.GridLayout;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class Window extends JFrame{
 
@@ -17,6 +20,26 @@ public class Window extends JFrame{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(900, 800);
 		setLocationRelativeTo(null);
+		
+		setLayout(new GridLayout(20, 20)); //20X20 lauks //velak varbut samainit uz pielāgojamu izmēru
+
+		for (int i = 0; i < atbildesBoard.length; i++) {
+		    for (int j = 0; j < atbildesBoard[i].length; j++) {
+		        char letter = atbildesBoard[i][j];
+		        
+		        JTextField cell = new JTextField();
+		        cell.setHorizontalAlignment(SwingConstants.CENTER); // teksta nocentrēšana
+		        
+		        if (letter == '-') {
+		            cell.setBackground(java.awt.Color.GRAY);
+		            cell.setEditable(false);
+		        } else {
+		            cell.setBackground(java.awt.Color.WHITE);
+		            cell.setEditable(true);
+		        }
+		        add(cell);
+		    }
+		}
 		
 	}
 }
