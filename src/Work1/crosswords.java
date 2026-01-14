99a98e94253aab5598944cdd1029356fee703a1b
P0if <marcisabele16@gmail.com>
2026-01-14 03:31:44 +0200

    ievietoju comandu connecton conn lai spētu savienoties ar datubazi un conn = 0 tas ir kad nezidodas.

--- src/Work1/crosswords.java
+++ src/Work1/crosswords.java
@@ -5,4 +5,8 @@ import javax.swing.JOptionPane;
 
 public class crosswords {
 
+	  public static void main(String[] args) {
+	        Connection conn = database.getConnection();
+	        if (conn == null) return;
+	        
 }
