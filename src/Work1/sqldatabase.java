6cc0ee8229b0fe11fc32ae0d9b190f179f09f450
P0if <marcisabele16@gmail.com>
2026-01-14 02:07:31 +0200

    pievienoju standardcharset lai saprastu UTF-8 latviešuburtus, ka ari failes, un path lai spētu nolasit un ceļu uz sql

--- src/Work1/sqldatabase.java
+++ src/Work1/sqldatabase.java
@@ -2,4 +2,15 @@ package Work1;
 
 public class sqldatabase {
 
+	import java.sql.*;
+	import java.util.ArrayList;
+	import java.util.List;
+	import javax.swing.JOptionPane;
+	import java.nio.charset.StandardCharsets;
+	import java.nio.file.Files;
+	import java.nio.file.Path;
+	
+	
+	
+	
 }
