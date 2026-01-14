4ce507ac1972b26dc18df645284830efa478ea4a
P0if <marcisabele16@gmail.com>
2026-01-14 11:49:47 +0200

    pievienoju metodi, kas palaiž krustvārdu mīklu ar datiem no DB.pec ta es ar arraylist sagatavoju strukturu krustvārdu miklai word def un ar for ciklu eju cauri viisiem jautajumiem un ar world add no katra ieraksta paņemu vārdu un defs paņem jautajumu un pievino to texta ar numuriem gridu man vel ir kaustaisa kas apkopos un defines nozimes dažam komandam

--- src/Work1/Service.java
+++ src/Work1/Service.java
@@ -1,5 +1,30 @@
 package Work1;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
 public class Service {
 
+    public static void startFromDatabase(Connection c, String lvl, int n) {
+
+        try {
+            
+            ArrayList<Entry> data = Dictionary.getByLevel(c, lvl, n);
+
+           
+            ArrayList<String> words = new ArrayList<>();
+            String defs = "";
+
+            for (int i = 0; i < data.size(); i++) {
+                words.add(data.get(i).getWord());
+                defs += (i + 1) + ". " + data.get(i).getDef() + "\n";
+            }
+
+           
+            CrosswordGrid.grid(words, defs);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
