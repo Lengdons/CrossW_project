b76a7d503d5aaeff70d6b2f28551bd351f0b852d
P0if <marcisabele16@gmail.com>
2026-01-14 04:33:01 +0200

    pievienoju konstruktorus lai spētu java atščirt jautajumu un vardu vienu no otra

--- src/Work1/Entry.java
+++ src/Work1/Entry.java
@@ -1,5 +1,16 @@
 package Work1;
 
 public class Entry {
+	
+	    private final String word;
+	    private final String def;
 
-}
+	    public Entry(String w, String d) {
+	        word = w;
+	        def = d;
+	    }
+
+	    public String getWord() { return word; }
+	    public String getDef() { return def; }
+	}
+
