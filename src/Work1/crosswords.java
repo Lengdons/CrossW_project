8b60a92db5e6035dd1c2c1ef24cf4b21435ef626
P0if <marcisabele16@gmail.com>
2026-01-14 03:39:22 +0200

    joption dod izveli prieks grutibas pakapes un pedejajs ir par palaisanu ar datubazi un izvelk tik 10 jautajumus un parada tos

--- src/Work1/crosswords.java
+++ src/Work1/crosswords.java
@@ -4,9 +4,20 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 
 public class crosswords {
+public static void main(String[] args) {
+Connection conn = sqldatabase.getConnection();
+ if (conn == null) return;
+	  
+String inp = JOptionPane.showInputDialog(
+ "Izvēlies grūtību:\n1 - Viegli\n2 - Vidēji\n3 - Grūti");
 
-	  public static void main(String[] args) {
-	        Connection conn = database.getConnection();
-	        if (conn == null) return;
-	        
-}
+	 if (inp == null) return;
+
+	 String lvl =
+	  inp.equals("1") ? "viegli" :
+	  inp.equals("2") ? "videji" : "gruti";
+CrosswordService.startFromDatabase(conn, lvl, 10);
+	    }
+	}
+
+	  
