5bf1d0fac59142cb0649c20db5a9c2f2a2040efb
P0if <marcisabele16@gmail.com>
2026-01-14 04:41:02 +0200

    iestadiju getbylevel kas paņem vārdus no DB

--- src/Work1/Dictionary.java
+++ src/Work1/Dictionary.java
@@ -1,5 +1,15 @@
 package Work1;
+ 
 
-public class Dictionary {
+	import java.sql.*;
+	import java.util.ArrayList;
 
-}
+	public class Dictionary {
+
+	    public static ArrayList<Entry> getByLevel(Connection c, String lvl, int n)
+	            throws SQLException {
+
+	        ArrayList<Entry> list = new ArrayList<>();
+	        String sql = "SELECT vards, definicija FROM vards WHERE lvl=? LIMIT ?";
+
+	       
